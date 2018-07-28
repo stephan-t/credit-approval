03800a3c864721734aab47e559078a893d9e6a97
stephan-t <40370954+stephan-t@users.noreply.github.com>
2018-07-28 01:14:52 -0400

    Initial commit

---
+++ classifier_cross-valid.R
@@ -0,0 +1,51 @@
+# Import packages
+library(rpart)
+
+
+#### Classification ####
+
+# Train and test model using k-fold cross validation
+set.seed(50)
+data.rand <- data[sample(1:nrow(data)), ]  # Randomize data set
+k <- 10  # Number of folds
+fold <- round(nrow(data.rand) / k, 0)  # Size of fold
+idx.head <- 0
+idx.tail <- fold
+dt.cor <- 0
+
+# Set fold in iteration i as test set and remaining folds as training set
+for (i in 1:k) {
+  idx.head <- idx.head + 1
+  idx.tail <- fold * i
+  idx <- idx.head:idx.tail
+  
+  # Split data for training and testing
+  data.test <- na.omit(data.rand[idx,])
+  data.train <- data.rand[-idx,]
+  
+  # Build model
+  dt.model <- rpart(V16 ~ ., data = data.train, method="class", parms = list(split="information"))
+    
+  # Test model
+  dt.pred <- predict(dt.model, data.test[, names(data.test) != "V16"], type="class")
+
+  
+  #### Evaluation ####
+  
+  # Create data frame of actual and predicted classes
+  dt.pred.df <- data.test
+  dt.pred.df <- cbind(dt.pred.df, predict=dt.pred)
+  names(dt.pred.df)[names(dt.pred.df) == "V16"] <- "actual"
+  
+  # Create confusion matrix
+  dt.cm <- table(dt.pred.df$actual, dt.pred.df$predict, dnn=c("Actual","Predicted"))
+  
+  # Count correct classifications
+  dt.cor <- dt.cor + sum(diag(dt.cm))
+  
+  # Set current fold's tail as next fold's head 
+  idx.head <- idx.tail
+}
+
+# Calculate accuracy
+(dt.accu <- dt.cor / nrow(data))
